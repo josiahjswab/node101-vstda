c85b74f4d4ec3618aea77275964abac8f28b663a
Josiah Swab <josiahjswab@gmail.com>
2019-12-17 16:39:43 -0800

    Initialize

--- server/app.js
+++ server/app.js
@@ -3,6 +3,44 @@ const morgan = require('morgan');
 
 const app = express();
 
-// add your code here
+app.use(morgan('dev'));
+
+const fakeTodos = 
+[
+    {
+        todoItemId: 0,
+        name: 'an item',
+        priority: 3,
+        completed: false
+    },
+    {
+        todoItemId: 1,
+        name: 'another item',
+        priority: 2,
+        completed: false
+    },
+    {
+        todoItemId: 2,
+        name: 'a done item',
+        priority: 1,
+        completed: true
+    }
+];
+
+app.get('/', (req, res) => {
+  res.status(200).send('ok')
+})
+
+app.get('/api/TodoItems', (req, res) => {
+  res.status(200).send(fakeTodos);
+})
+
+app.get('/api/TodoItems/:id', (req, res) => {
+  if(fakeTodos[req.params.id]) {
+    res.status(200).send(fakeTodos[req.params.id]);
+  } else {
+    res.status(400).send('This todo doesn\'t exist.')
+  }
+})
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
 const server = require('./app');
 
 // write your code here
+const PORT = process.env.PORT || 8484;
+
+server.listen(PORT, console.log(`Listening on http://localhost:${PORT}`));
